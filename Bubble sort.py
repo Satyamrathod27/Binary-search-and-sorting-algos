cb2e225a5d2fc930e03ec6e10e334ad1554009a6
Satyam Rathod <satyamrathod57@gmail.com>
2026-02-19 17:59:17 +0530

    Implement descending bubble sort
    Added implementation for descending bubble sort.

--- Bubble sort.py
+++ Bubble sort.py
@@ -9,3 +9,17 @@ for i in range(n - 1):              # number of passes
             arr[k], arr[k + 1] = arr[k + 1], arr[k]
 
 print(arr)  # [1, 2, 3, 4, 5]
+
+# descending bubble sort
+nums = [5,7,8,4,1,9,2]
+
+n = len(nums)
+
+for i in range(0,n-1):
+   for j in range(0,n-1-i):
+       if nums[j+1]>nums[j]:
+           nums[j+1],nums[j]=nums[j],nums[j+1]
+
+
+print(nums)
+
