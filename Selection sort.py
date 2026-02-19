b9caa5bfe73578ec1f2e7b2fd315cc95ba99b50b
Satyam Rathod <satyamrathod57@gmail.com>
2026-02-19 17:45:49 +0530

    Implement selection sort for descending order
    Added implementation for selection sort in descending order.

--- Selection sort.py
+++ Selection sort.py
@@ -8,3 +8,17 @@ for i in range(n):
             arr[k], arr[i] = arr[i], arr[k]
 
 print(arr)  # [1, 2, 3, 4, 5]
+
+# for decending order
+nums = [5,7,8,4,1,6,9,2]
+
+n = len(nums)
+for i in range(0,n):
+   maxi = i
+   for j in range(i+1,n):
+       if nums[maxi]<nums[j]:
+           maxi = j
+   nums[i],nums[maxi]=nums[maxi],nums[i]
+
+print(nums)
+
