30da71ae11c258e988d3863180484f49c643559f
Satyam Rathod <satyamrathod57@gmail.com>
2026-02-20 23:04:30 +0530

    Add merge_array function for merge sort
    Implement merge_array function for merging two sorted arrays.

---
+++ merge sort.py
@@ -0,0 +1,23 @@
+def merge_array(left,right):
+   result=[]
+   i,j=0,0
+   n,m = len(left),len(right)
+   while i <n and j<m:
+       if left[i]<=right[j]:
+           result.append(left[i])
+           i+=1
+       else:
+           result.append(right[j])
+           j+=1
+
+   if i < n:
+       while i<n:
+           result.append(left[i])
+           i+=1
+   if j<m:
+       while j<m:
+           result.append(right[j])
+           j+=1
+
+   return result
+
