7fe02b2a1e04bc84da00ebe482c25c41a10e6a67
Satyam Rathod <satyamrathod57@gmail.com>
2026-02-20 23:03:26 +0530

    Implement insertion sort for a new list of numbers
    Added a new insertion sort implementation for a different array.

--- insertion sort.py
+++ insertion sort.py
@@ -13,3 +13,15 @@ for i in range(1, len(arr)):
     arr[j + 1] = key
 
 print(arr)   # [1, 2, 3, 4, 5]
+
+nums = [3,5,6,4,8,9,10,7,1,2]
+n = len(nums)
+for i in range(1,n):
+   key = nums[i]
+   j = i-1
+   while j>=0 and nums[j]>key:
+       nums[j+1]=nums[j]
+       j-=1
+   nums[j+1]= key
+
+print(nums)
